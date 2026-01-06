498f635ddbbe1c9dc21b9e1626f54decffbee43f
tony <tony940804tony@gmail.com>
2026-01-06 22:19:30 +0800

    final-1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction() {
   var menuBtn = document.getElementById("myNavMenu");
 
@@ -9,7 +8,6 @@ function myMenuFunction() {
   }
 }
 
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
 window.onscroll = function () {
   headerShadow();
 };
@@ -27,63 +25,6 @@ function headerShadow() {
     navHeader.style.lineHeight = "90px";
   }
 }
-
-// /* ----- TYPING EFFECT ----- */
-// var typingEffect = new Typed(".typedText", {
-//   strings: ["Designer", "Youtuber", "Developer"],
-//   loop: true,
-//   typeSpeed: 100,
-//   backSpeed: 80,
-//   backDelay: 2000,
-// });
-
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
-
-// /* -- HOME -- */
-// sr.reveal(".featured-text-card", {});
-// sr.reveal(".featured-name", { delay: 100 });
-// sr.reveal(".featured-text-info", { delay: 200 });
-// sr.reveal(".featured-text-btn", { delay: 200 });
-// sr.reveal(".social_icons", { delay: 200 });
-// sr.reveal(".featured-image", { delay: 300 });
-
-// /* -- PROJECT BOX -- */
-// sr.reveal(".project-box", { interval: 200 });
-
-// /* -- HEADINGS -- */
-// sr.reveal(".top-header", {});
-
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-// /* -- ABOUT INFO & CONTACT INFO -- */
-// const srLeft = ScrollReveal({
-//   origin: "left",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
-
-// srLeft.reveal(".about-info", { delay: 100 });
-// srLeft.reveal(".contact-info", { delay: 100 });
-
-// /* -- ABOUT SKILLS & FORM BOX -- */
-// const srRight = ScrollReveal({
-//   origin: "right",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
-
-// srRight.reveal(".skills-box", { delay: 100 });
-// srRight.reveal(".form-control", { delay: 100 });
-
-/* ----- CHANGE ACTIVE LINK ----- */
 
 const sections = document.querySelectorAll("section[id]");
 
